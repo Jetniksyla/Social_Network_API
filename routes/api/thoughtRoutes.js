f2d6890aa646bd516300cc9a34ad221242b61c18
Jetnik Syla <sjetnik@gmail.com>
2024-04-26 15:55:17 -0400

    define the controllers  in the correct directory folder and addedthe routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,175 +1,27 @@
-const { Thought, Reaction, User } = require("../models");
+const router = require("express").Router();
 
-// --------/api/thoughts---------
 
-const thoughtController = {
-  // GET to get all thoughts
-  async getThought(req, res) {
-    try {
-      const thoughtData = await Thought.find();
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
 
-      res.status(200).json(thoughtData);
-    } catch (err) {
-      res
-        .status(500)
-        .json({ message: "Failed to retrieve thoughts due to server error" });
-    }
-  },
+} = require('../../controllers/thoughtController');
 
-  // GET to get a single thought by its _id
-  async getSingleThought(req, res) {
-    try {
-      const getThoughtId = await Thought.findOne({
-        _id: req.params.thoughtId,
-      }).select("-__v").populate("reactions");
-      if (!getThoughtId) {
-        res.status(404).json({ message: "No thought found with that ID" });
-      } else {
-        res.status(200).json(getThoughtId);
-        console.log("Reaction count:", getThoughtId.reactionCount);
-      }
-    } catch (err) {
-      console.log(err);
-      res
-        .status(500)
-        .json({ message: "Failed to retrieve thought due to server error" });
-    }
-  },
-  // POST to create a new thought
-  async createThought(req, res) {
-    try {
-      // Create the new thought
-      const newThought = await Thought.create(req.body);
+// /api/thoughts
+router.route('/').get(getThought).post(createThought);
 
-      // If thought is created successfully, link it to the user
-      if (!newThought) {
-        res
-          .status(404)
-          .json({ message: "Failed to create thought due to invalid input" });
-      } else {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: req.body.userId },
-          { $push: { thoughts: newThought._id } },
-          { new: true }
-        );
-        // Send the original thought data, not the user update data
-        res.status(200).json({
-          message: "Thought has been created successfully and linked to user",
-          thought: newThought,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "Failed to create thought due to server error" });
-    }
-  },
-  // PUT to update a thought by its _id
-  async updateThought(req, res) {
-    try {
-      const updatedThought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtId },
-        { $set: req.body },
-        { runValidators: true, new: true } // Ensures updated document is returned
-      );
-      if (!updatedThought) {
-        res.status(404).json({ message: "No thought with this id!" });
-      } else {
-        res.status(200).json({
-          message: "Thought has been updated successfully",
-          thoughts: updatedThought,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "Failed to update thought due to server error" });
-    }
-  },
-  // DELETE to remove a thought by its _id
-  async deleteThought(req, res) {
-    try {
-      const removedThought = await Thought.findOneAndDelete({
-        _id: req.params.thoughtId,
-      });
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-      if (!removedThought) {
-        res.status(404).json({ message: "No thought with this id!" });
-      } else {
-        // Update the user after the thought has been deleted
-        const updatedUser = await User.findOneAndUpdate(
-          { thoughts: req.params.thoughtId },
-          { $pull: { thoughts: req.params.thoughtId } },
-          { new: true }
-        );
-        res.status(200).json({
-          message: "Thought has been deleted successfully and user updated.",
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "Failed to delete thought due to server error" });
-    }
-  },
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
 
-  // -------/api/thoughts/:thoughtId/reactions--------
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
-  // POST to create a reaction stored in a single thought's reactions array field
-  async addReaction(req, res) {
-    try {
-      const updatedThought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtId },
-        {
-          $addToSet: { reactions: req.params.reactionId },
-          $inc: { reactionCount: 1 },
-        },
-        { runValidators: true, new: true }
-      );
-
-      if (!updatedThought) {
-        res.status(404).json({ message: "No thought found with this ID" });
-      } else {
-        res.status(200).json({
-          message: "Reaction added successfully",
-          thoughts: updatedThought,
-        });
-        console.log("Reaction count:", updatedThought.reactionCount);
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        message: "Failed to add reaction due to server error",
-        error: err.message, // Include this line in development mode for more detailed debugging information
-      });
-    }
-  },
-  // DELETE to pull and remove a reaction by the reaction's reactionId value
-  async removeReaction(req, res) {
-    try {
-      const removedThought = await Thought.findOneAndUpdate(
-        { _id: req.params.thoughtId },
-        { $pull: { reactions: { _id: req.params.reactionId } } }, // Assuming reactions is an array of objects
-        { runValidators: true, new: true }
-      );
-
-      // Check if the thought was found and updated
-      if (!removedThought) {
-        res.status(404).json({ message: "No thought found with this ID" });
-      } else {
-        // Optionally, check if the reaction was actually removed, this would require knowing the state before and after the update
-        res.status(200).json({ message: "Reaction removed successfully" });
-      }
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "Failed to remove reaction due to server error" });
-    }
-  },
-};
-
-module.exports = thoughtController;
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,193 +1,22 @@
-const { User, Thought, Reaction } = require("../models");
+const router = require("express").Router();
 
-// ----------/api/users-----------
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/userController");
 
-const userController = {
-  // GET to get all users
-  async getUser(req, res) {
-    try {
-      const userData = await User.find({});
-      if (!userData || userData === 0) {
-        res.status(404).json({ message: "No users found" });
-      } else {
-        res.status(200).json(userData);
-      }
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "Failed to retrieve users due to server error" });
-    }
-  },
+// /api/users
+router.route("/").get(getUser).post(createUser);
 
-  // GET to get a single user by its _id
-  async getSingleUser(req, res) {
-    try {
-      const getUserById = await User.findOne({ _id: req.params.userId })
-        .select("-__v")
-        .populate("friends")
-        .populate("thoughts");
-      if (!getUserById) {
-        return res.status(404).json({ message: "No user found with this Id" });
-      } else {
-        res.status(200).json(getUserById);
-      }
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "Failed to retrieve user due to server error" });
-    }
-  },
+// /api/users/:id
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-  // POST to create a new user
-  async createUser(req, res) {
-    try {
-      const newUser = await User.create(req.body);
-      if (!newUser) {
-        res
-          .status(404)
-          .json({ message: "Failed to create user due to invalid input" });
-      } else {
-        res.status(200).json(newUser);
-      }
-    } catch (err) {
-      console.error(err);
-      res
-        .status(500)
-        .json({ message: "Failed to create user due to server error" });
-    }
-  },
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
-  // PUT to update amuser by its _id
-  async updateUser(req, res) {
-    try {
-      const updateUser = await User.findOneAndUpdate(
-        { _id: req.params.userId },
-        { $set: req.body },
-        { runValidators: true, new: true }
-      );
-      if (!updateUser) {
-        res.status(404).json({ message: "No user with this id!" });
-      } else {
-        res.status(200).json(updateUser);
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "User has been updated successfully" });
-    }
-  },
-
-  // DELETE to remove a user by its _id
-  async deleteUser(req, res) {
-    try {
-      const deleteUser = await User.findOneAndDelete({
-        _id: req.params.userId,
-      });
-      if (!deleteUser) {
-        return res.status(404).json({ message: "No user found with this ID!" });
-      } else {
-        // Assuming deleteUser.thoughts is an array of thought IDs
-        if (deleteUser.thoughts && deleteUser.thoughts.length > 0) {
-          await Thought.deleteMany({ _id: { $in: deleteUser.thoughts } });
-        }
-        res.status(200).json({
-          message: "User and associated thoughts deleted successfully",
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        message:
-          "Failed to delete user or associated thoughts due to server error",
-      });
-    }
-  },
-
-  // ----------/api/users/:userId/friends/:friendId-----------
-
-  // POST to add a new friend to a user's friend list
-  async addFriend(req, res) {
-    try {
-      // Prevent users from adding themselves as a friend
-      if (req.params.userId === req.params.friendId) {
-        return res.status(400).json({
-          message: "Cannot add yourself as a friend",
-        });
-      }
-
-      // Check if the friend ID is valid
-      const friendExists = await User.findById(req.params.friendId);
-      if (!friendExists) {
-        return res.status(404).json({
-          message: "Friend not found",
-        });
-      }
-
-      const addUserAsFriend = await User.findOneAndUpdate(
-        { _id: req.params.userId },
-        { $addToSet: { friends: req.params.friendId } },
-        { runValidators: true, new: true }
-      );
-
-      if (!addUserAsFriend) {
-        return res.status(404).json({
-          message: "User not found",
-        });
-      } else {
-        res.status(200).json({
-          message: "Friend added successfully",
-          user: addUserAsFriend,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        message: "Failed to add friend due to server error",
-        error: err.message, // Provide error details in the response for debugging
-      });
-    }
-  },
-
-  // DELETE to remove a friend from a user's friend list
-  async removeFriend(req, res) {
-    try {
-      const userBeforeUpdate = await User.findById(req.params.userId);
-      if (!userBeforeUpdate) {
-        return res.status(404).json({
-          message: "User not found",
-        });
-      }
-
-      const removeFriend = await User.findOneAndUpdate(
-        { _id: req.params.userId },
-        { $pull: { friends: req.params.friendId } },
-        { runValidators: true, new: true }
-      );
-
-      if (!removeFriend) {
-        return res.status(404).json({
-          message: "User not found",
-        });
-      } else {
-        const wasRemoved = !userBeforeUpdate.friends.includes(
-          req.params.friendId
-        );
-        res.status(200).json({
-          message: wasRemoved
-            ? "Friend removed successfully"
-            : "No friend was removed, may not have been on the list",
-          user: removeFriend,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        message: "Failed to remove friend due to server error",
-        error: err.message,
-      });
-    }
-  },
-};
-
-module.exports = userController;
+module.exports = router;
